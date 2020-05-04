d133f2d44ed000f8c9556852593a50a8a353a5fe
Joshua Book <joshua.book@gmail.com>
2020-05-04 17:07:56 -0400

    WIP - adding child and sibling SwiftUI views of CreationsList.

---
+++ DM-iOS-Template/Creations/CreationDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  CreationDetailsView.swift
+//  DM-iOS-Template
+//
+//  Created by Joshua Book on 5/4/20.
+//  Copyright © 2020 PagerXYZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreationDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreationDetailsView()
+    }
+}
---
+++ DM-iOS-Template/Creations/CreationsListItemView.swift
@@ -0,0 +1,32 @@
+//
+//  CreationsListItemView.swift
+//  DM-iOS-Template
+//
+//  Created by Joshua Book on 5/4/20.
+//  Copyright © 2020 PagerXYZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreationsListItemView: View {
+  var body: some View {
+    Text("hello world")
+//    VStack(alignment: .leading) {
+//      AsyncImage(
+//        urlString: creation.photos[0].url,
+//        placeholder: Text("Loading ..."),
+//        cache: self.cache,
+//        configuration: { $0.resizable() }
+//      ).aspectRatio(contentMode: .fit)
+//      Text(creation.name)
+//        .font(.title)
+//      Text(creation.summary ?? "")
+//    }
+  }
+}
+
+struct CreationsListItemView_Previews: PreviewProvider {
+  static var previews: some View {
+      CreationsListItemView()
+  }
+}
